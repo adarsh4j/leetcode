f4a9482e2c0f889d9051c6c9891a0fdcd22192ab
Nimithaps8 <72964747+Nimithaps8@users.noreply.github.com>
2023-10-02 12:55:15 +0530

    Add files via upload

---
+++ Binary Search1.c
@@ -0,0 +1,22 @@
+int search(int* nums, int numsSize, int target){
+    int begin=0,end=numsSize-1,mid;
+    while(begin<=end)
+    {
+        mid=begin+end /2;
+        if(*(nums+mid)==target)
+        {
+            return mid;
+        }
+
+        if(*(nums+mid) > target)
+        {
+            end=mid-1;
+        }
+        else
+        {
+            begin=mid+1;
+        }
+       
+    }
+ return -1;
+}
